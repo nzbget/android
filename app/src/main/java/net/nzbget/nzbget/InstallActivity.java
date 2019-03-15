8b0817ed56d0985644608de0963c671c4d4d17ba
Andrey Prygunkov <hugbug@users.sourceforge.net>
2019-03-15 23:20:31 +0100

    using https for accessing nzbget.net
    This also fixes possible download failures on Android 9 due to security
    restrictions.

--- app/src/main/java/net/nzbget/nzbget/InstallActivity.java
+++ app/src/main/java/net/nzbget/nzbget/InstallActivity.java
@@ -179,7 +179,7 @@ public class InstallActivity extends AppCompatActivity implements ActivityCompat
         File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "nzbget-version-android.json");
         file.delete();
 
-        String url = "http://nzbget.net/info/nzbget-version-android.json";
+        String url = "https://nzbget.net/info/nzbget-version-android.json";
         DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
         request.setTitle("NZBGet daemon installer info");
         request.setDescription("nzbget-version-android.json");
